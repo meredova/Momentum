39844129aee698073e1161af797a7bcadb23c03f
meredova <101661046+meredova@users.noreply.github.com>
2024-05-24 17:17:24 +0300

    Update quotes.js into Eng lang

--- js/quotes.js
+++ js/quotes.js
@@ -1,39 +1,39 @@
 const quotes = [
   {
-    "text": "Пишите код так, как будто сопровождать его будет склонный к насилию психопат, который знает, где вы живете",
-    "author": "Стив Макконнелл"
+    "text": "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.",
+    "author": "Ellen Ullman"
   },
   {
-    "text": "Сложность программы растет до тех пор, пока не превысит способности программиста",
-    "author": "Артур Блох. Законы Мэрфи"
+    "text": "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    "author": "Patrick McKenzie"
   },
     {
-    "text": "Ходить по воде и разрабатывать программы, следуя ТЗ, очень просто… если они заморожены",
-    "author": "И. Берард"
+    "text": "Rules of Optimisation: Rule 1: Don't do it. Rule 2 (for experts only): Don't do it yet.",
+    "author": "Michael A. Jackson"
   },
   {
-    "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили",
-    "author": "Mosher’s Law of Software Engineering"
+    "text": "No one in the brief history of computing has ever written a piece of perfect software. It's unlikely that you'll be the first.",
+    "author": "Andy Hunt"
   },
   {
-    "text": "Иногда лучше остаться спать дома в понедельник, чем провести всю неделю в отладке написанного в понедельник кода",
-    "author": "К. Томсон"
+    "text": "One of the best programming skills you can have is knowing when to walk away for a while.",
+    "author": "Oscar Godson"
   },
   {
-    "text": "Измерять продуктивность программиста подсчетом строк кода — это так же, как оценивать постройку самолета по его весу",
-    "author": "Б. Гейтс"
+    "text": "It's hard enough to find an error in your code when you're looking for it; it's even harder when you've assumed your code is error-free.",
+    "author": "Steve McConnell"
   },
   {
-    "text": "Многие из вас знакомы с достоинствами программиста. Их всего три, и разумеется это: лень, нетерпеливость и гордыня",
-    "author": "Л. Уолл"
+    "text": "The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.",
+    "author": "Tom Cargill"
   },
   {
-    "text": "Если вы дадите человеку программу, то займете его на один день. Если вы научите человека программировать, то займете его на всю жизнь",
-    "author": "У. Латиф"
+    "text": "Good code is its own best documentation. As you're about to add a comment, ask yourself, `How can I improve the code so that this comment isn't needed?` Improve the code and then document it to make it even clearer.",
+    "author": "Steve McConnell"
   },
   {
-    "text": "Работает? Не трогай.",
-    "author": "Любой программист"
+    "text": "One of my most productive days was throwing away 1000 lines of code.",
+    "author": "Ken Thompson"
   },
 ]  
 
